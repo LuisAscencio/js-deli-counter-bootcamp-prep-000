0d58636cd0730ea2e9fac6c1edebc60f6ff9cdd3
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-25 16:09:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber (katzDeliLine, name){
   katzDeliLine.push(name)
-  var position = katzDeliLine.length +  1
+  var position = katzDeliLine.length
   return "Welcome, " + name +". You are number "+ position +" in line."
 }
 
