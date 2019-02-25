27d466767cd2880082d10c24ba15c1d4d3c01fa6
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-25 14:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function nowServing (katzDeliLine){
 
 
 function currentLine(line){
-  if(!line.length) {
+  if(line.length===0) {
     return "The line is currently empty.";
   }
   var nLM = [];
